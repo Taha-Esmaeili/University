1f97fa675f501fda94c70930ac8e803b50c9dba4
Taha <140310441+Taha-Esmaeili@users.noreply.github.com>
2024-08-05 17:06:14 +0330

    Create task2.py KMeans clustering

---
+++ Advanced Programming/KMeans Clustering algorithm/task2.py
@@ -0,0 +1,22 @@
+import pandas as pd 
+
+# Read dataset
+initial_df = pd.read_csv('Dry_Bean.csv')
+
+# Drop categorical feature
+df = initial_df.drop('Class',axis=1,inplace=False)
+
+# Get corr table
+corr_matrix = df.corr()
+
+# Filter the table
+selected = (corr_matrix>0.8)&(corr_matrix!=1)
+
+# Get filtered rows
+high_corr_feature=corr_matrix[selected]
+
+# Create final table
+high_corr_feature_df=high_corr_feature.reset_index().rename(columns={'index':'feature_1'})
+high_corr_feat_tidy=pd.melt(high_corr_feature_df,id_vars=['feature_1'],var_name='feature_2',value_name='correlation_coefficient')
+
+print(high_corr_feat_tidy.dropna())
